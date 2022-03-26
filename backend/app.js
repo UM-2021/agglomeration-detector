f81fdde2bc1e2525f8ffb918638e86f2c56af7ec
jmstaricco <juan.staricco@decemberlabs.com>
2022-03-26 20:42:27 -0300

    first server configs

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+
+const signsRouter = require('./routes/signRoutes');
+const usersRouter = require('./routes/userRouter');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
+app.options('*', cors());
+
+app.use(helmet());
+
+app.use('/api/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on the server`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
