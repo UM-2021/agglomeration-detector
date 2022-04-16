476a21600490c57f5b1ab027cfda9f301d7ecd68
Jose Algorta <jpalgorta@correo.um.edu.uy>
2022-04-16 15:55:54 -0300

    Fix url for axios calls.

--- frontend/src/middlewares/axios.js
+++ frontend/src/middlewares/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: `http${process.env.NODE_ENV === 'production' ? 's' : ''}://${
     process.env.REACT_APP_IP_ADDRESS
-  }:3000`
+  }`
 });
 
 export default instance;
